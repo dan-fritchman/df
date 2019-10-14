1692207812da3d11d8ac7483a59e638f94418969
Dan Fritchman <dan@fritch.mn>
2019-10-14 21:12:34 +0200

    Add zshrc (from conda), linking script

---
+++ ln.py
@@ -0,0 +1,16 @@
+import os 
+from pathlib import Path 
+
+dotfiles = [
+	'.profile', 
+	'.zprofile',
+	'.zshrc',
+	'.bash_profile',
+]
+dotfiles = [Path(d).absolute() for d in dotfiles]
+
+for d in dotfiles:
+	os.chdir(os.environ['HOME'])
+	os.system(f'ln -fs {d}')
+
+
